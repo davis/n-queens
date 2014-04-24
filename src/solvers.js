d93eb6ab8314fddc52b3ede9be59f9dab84482a1
Hack Reactor Students <dchk90@gmail.com>
2014-04-23 22:14:30 -0700

    updates solvers to solve Rook problems

--- src/solvers.js
+++ src/solvers.js
@@ -14,9 +14,59 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = []; //fixme
+  var counter = 0;
+
+  console.log('new board of size', n)
+  var b = new Board({'n': n});
+
+  var recursion = function(rowIndex) {
+    // base case returns at end
+    if(rowIndex === n) {
+      var copy = [];
+      for(var i = 0; i < n; i++) {
+        copy.push(b.rows()[i].slice());   // stupid javascript
+      }
+      console.log('pushing a solution')
+      counter++;
+
+      console.log(copy)
+      // solution.push(Array.slice(b.rows()));
+    }
+
+    // recursive case
+    else {
+      for(var i = 0; i < n; i++) {
+
+
+        console.log('toggling', rowIndex, i)
+        console.log('before', b.get(rowIndex)[i])
+        b.togglePiece(rowIndex, i);       // try toggling row
+        console.log('after', b.get(rowIndex)[i])
+
+        if(b.hasAnyRooksConflicts()) {      // if conflicts:
+          console.log('conflict found', rowIndex, i)
+          console.log('before', b.get(rowIndex)[i])
+          b.togglePiece(rowIndex, i);     // toggle it back
+          console.log('after', b.get(rowIndex)[i])
+        }
+
+        else {
+          console.log('beginning recursion on row', rowIndex+1)
+          recursion(rowIndex+1);          // if no conflicts, recurse on next row
+          console.log('toggling', rowIndex, i, '(back)')
+          console.log('before', b.get(rowIndex)[i])
+          b.togglePiece(rowIndex, i);
+          console.log('after', b.get(rowIndex)[i])
+        }
+      }
+    }
+  };
+
+  recursion(0);
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  console.log(counter)
   return solution;
 };
 
